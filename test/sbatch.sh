bba7b989393497c1024dc83851116311c6c34ba9
Mingyoung Jeng <mingyoungjeng@gmail.com>
2023-08-30 21:51:08 -0500

    prepping cluster

--- test/sbatch.sh
+++ test/sbatch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=qcc-q                      # Job name
+#SBATCH --job-name=qcc                      # Job name
 #SBATCH --partition=sixhour                 # Partition Name (Required)
 #SBATCH --mail-type=END,FAIL                # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=mingyoungjeng@ku.edu    # Where to send mail
@@ -8,9 +8,9 @@
 #SBATCH --time=0-06:00:00                   # Time limit days-hrs:min:sec
 #SBATCH --output=%j.log                     # Standard output and error log
 #SBATCH --gres=gpu                          # 1 GPU
-#SBATCH --constraint=q6000,hpe_int_24_192    # a100, q8000, q6000, v100
-#--constraint=v100,del_int_40_192    # a100, q8000, q6000, v100
-#--constraint=a100,del_int_48_256    # a100, q8000, q6000, v100
+#SBATCH --constraint=a100,del_int_48_256    # a100, q8000, q6000, v100
+# --constraint=q6000,hpe_int_24_192    # q6000
+#--constraint=v100,del_int_40_192    # v100
 pwd; hostname; date
 
 QCC=/home/m174j393/work/quantum-convolutional-classifier
@@ -20,9 +20,9 @@ module load conda/latest
 
 conda activate $QCC/.env
 
-echo "running QCC"
+echo "running QCC $1"
 
 cd $QCC/test
-qcc load $1 -o ./v100
+qcc load $1 --parallel
 
 date
