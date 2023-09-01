c57c2dcedcc98a585298b8b5b21802dfeeba8e46
Mingyoung Jeng <mingyoungjeng@gmail.com>
2023-09-01 12:39:41 -0500

    idk

--- test/run.sh
+++ test/run.sh
@@ -2,7 +2,7 @@ CWD=/home/m174j393/work/quantum-convolutional-classifier/test
 REPEAT=1
 
 for file in $CWD/presentation/**/*.toml; do
-    for i in $(seq 1 $REPEAT); do
+    for i in ((i = 0; i < REPEAT; i++)); do
         sbatch $CWD/sbatch.sh $file
     done
 done
