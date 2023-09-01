a3889fda7fe7c5ce0780042699fd0c2528ef2e3d
Mingyoung Jeng <mingyoungjeng@gmail.com>
2023-09-01 12:23:07 -0500

    better run.sh to cover for out of memory

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,10 @@
 CWD=/home/m174j393/work/quantum-convolutional-classifier/test
+REPEAT=1
 
 for file in $CWD/presentation/**/*.toml; do
-    sbatch $CWD/sbatch.sh $file
+    for i in $(seq 1 $REPEAT); do
+        sbatch $CWD/sbatch.sh $file
+    done
 done
 
 squeue -u m174j393
